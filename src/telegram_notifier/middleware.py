42fa9e1c262d846a2984ae8492c3a56d4a5f6b7f
Jakhongir <ganiyevuzb@gmail.com>
2026-03-10 05:50:53 +0500

    feat: add async support to middleware

--- src/telegram_notifier/middleware.py
+++ src/telegram_notifier/middleware.py
@@ -1,13 +1,24 @@
+from asgiref.sync import iscoroutinefunction, markcoroutinefunction
+
 from telegram_notifier.report import report_exception
 
 
 class GlobalExceptionReporterMiddleware:
+    async_capable = True
+    sync_capable = True
+
     def __init__(self, get_response):
         self.get_response = get_response
+        if iscoroutinefunction(self.get_response):
+            markcoroutinefunction(self)
 
     def __call__(self, request):
         self._body = getattr(request, "body", b"")
         return self.get_response(request)
+
+    async def __acall__(self, request):
+        self._body = getattr(request, "body", b"")
+        return await self.get_response(request)
 
     def process_exception(self, request, exception):
         report_exception(exception, request, self._body)
